57771f46fc8f69c837bbd04f4443450df21369b5
Belen Rabel <belenrabel03@gmail.com>
2022-10-04 17:09:23 -0300

    Ingresamos los ultimos ejercicios en Python, Java y JavaScript

---
+++ javascript/leccion01/01-01-holamundo.js
@@ -0,0 +1,5 @@
+
+var nombre = "Belen";
+nombre = "Maria\nBelen\n";
+var apellido = "Rabel";
+console.log(nombre+apellido);
---
+++ python/Leccion 4/ejerciciorango.py
@@ -0,0 +1,19 @@
+#Ejercicio 1: Iterar en un rango de 0 a 10 e imprimir los divisibles entre 3
+#Ejemplo de ejecución 0,3,6,9
+print("rango de 0 A 10 con numeros divisibles entre 3")
+for i in range(11):
+    if i % 3 == 0:
+        print(i)
+
+#Ejercicio 2: Crear un rango de numeros de 2 a 6 e imrimelos
+#Ejemplo de ejecucion: 2,3,4,5,,6
+print("rango con valores de inicio = 2 y fin = 6")
+rango = range(2, 7)
+for i in range:
+    print(i)
+
+#Ejercicio 3: Crear un rango de 3 a 10 pero con incremento de 2 en 2, en lugar de 1 en 1
+#Ejemplo de ejecución 3,5,7,9
+print("Rango con valores de inicio = 3 y fin = 10, incremento = 2")
+for i in range: (3,11,2)
+print (i)
